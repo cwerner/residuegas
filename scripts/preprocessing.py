3892cd5161841f17a34627b02e2ee48ba40711fd
Christian Werner <christian.werner@kit.edu>
2022-03-30 16:27:07 +0200

    Add dvc pipeline with preprocessing and training steps

---
+++ scripts/preprocessing.py
@@ -0,0 +1,92 @@
+from pathlib import Path
+
+import pandas as pd
+import xarray as xr
+from dask_ml.model_selection import train_test_split
+
+from typing import Any, Dict
+
+SOURCE = Path("data") / "raw" 
+DEST = Path("data") / "processed" 
+
+def prepare_soil_variables() -> xr.Dataset:
+    soil_vars = "soc1,bd1,ph1,sand1,silt1,clay1".split(",")
+    soil_vars_clean = [x.replace("1","") for x in soil_vars]
+
+    soil = xr.open_dataset(SOURCE / "nc_rg_soil.nc")
+    soil = soil[soil_vars]
+    soil = soil.rename({k:v for k,v in zip(soil_vars, soil_vars_clean)})
+    return soil
+
+def prepare_mask() -> xr.Dataset:
+    return xr.open_dataset(SOURCE / "nc_rg_mask.nc")[["ID"]]
+
+
+def prepare_climate_variables(*, ref_dataset: xr.Dataset) -> xr.Dataset:
+    clim = xr.open_mfdataset([
+        SOURCE / "rg_arable_precipitation.nc", 
+        SOURCE / "rg_arable_temp-yearly.nc"])[['prec[mm]', 'temp_above_canopy[oC]']]
+    
+    clim = clim.rename({'prec[mm]': 'precip', 'temp_above_canopy[oC]': 'temp'})
+    clim = clim.reindex_like(ref_dataset)
+    clim = clim.sel(year=slice(2000,2099))
+    return clim
+
+def prepare_co2() -> Dict[str, Dict[Any, float]]:
+    source_files = [
+        "rg_arable_airchem_rcp45_1950-2100.txt.gz",
+        "rg_arable_airchem_rcp85_1950-2100.txt.gz",
+        ]
+    data = {}
+    for fname, rcp in zip(source_files, ["rcp4p5", "rcp8p5"]):
+        df = pd.read_csv(SOURCE / fname, skiprows=7, sep='\t')
+        df.columns = ['datetime', 'no3', 'nh4', 'co2']
+        df = df.astype({'datetime':'datetime64[ns]'})
+        
+        df_yearly = df.groupby(df.datetime.dt.year).first().drop(['datetime', 'no3', 'nh4'], axis=1).reset_index()
+        data[rcp] = pd.Series(df_yearly.co2.values, index=df_yearly.datetime).to_dict()
+    return data
+
+
+
+
+def merge_data() -> pd.DataFrame:
+    mask = prepare_mask()
+    soil = prepare_soil_variables()
+    climate  = prepare_climate_variables(ref_dataset=soil)
+    co2 = prepare_co2()
+
+    n2o = xr.open_dataset(SOURCE / "ensemble_n2o.nc")
+    gwp = xr.open_dataset(SOURCE / "ensemble_gwp.nc")
+
+    ds = xr.merge([mask, soil, climate, n2o, gwp])
+
+    df = ds.to_dataframe().dropna().reset_index()
+    df["co2"] = 0
+    df.loc[df.rcp == "RCP4.5", "co2"] = df.year.map(co2["rcp4p5"])
+    df.loc[df.rcp == "RCP8.5", "co2"] = df.year.map(co2["rcp8p5"])
+    return df
+
+
+def split_and_write_data(df: pd.DataFrame,*, seed=42) -> None:
+    df = df.drop(['ID', 'lat', 'lon', 'year'], axis= 1)
+
+    X_train, X_test, y_train, y_test = train_test_split(df.drop(['n2o', 'gwp'], axis=1), df[['n2o', 'gwp']], shuffle=True, train_size=0.8, random_state=seed)
+    y_train_n2o, y_train_gwp = y_train['n2o'], y_train['gwp']
+    #y_test_n2o, y_test_gwp = y_test['n2o'], y_test['gwp']
+
+    gzip_args = {'method': 'gzip', 'compresslevel': 1}
+    X_train.to_csv(DEST / "x_train.csv.gz", compression=gzip_args)
+    X_test.to_csv(DEST / "x_test.csv.gz", compression=gzip_args)
+    y_train.to_csv(DEST / "y_train.csv.gz", compression=gzip_args)
+    y_test.to_csv(DEST / "y_test.csv.gz", compression=gzip_args)
+    y_train_n2o.to_csv(DEST / "y_train_n2o.csv.gz", compression=gzip_args)
+    y_train_gwp.to_csv(DEST / "y_train_gwp.csv.gz", compression=gzip_args)
+
+
+def main():
+    df = merge_data()
+    split_and_write_data(df, seed=42)
+
+if __name__ == "__main__":
+    main()
--- scripts/train.py
+++ scripts/train.py
@@ -9,7 +9,7 @@ from dask_ml.model_selection import train_test_split
 import numpy as np
 import joblib
 import xarray as xr
-import hvplot.xarray
+#import hvplot.xarray
 from pathlib import Path
 import pandas as pd
 from sklearn.inspection import permutation_importance
@@ -20,16 +20,26 @@ import random
 
 from dask.distributed import Client, LocalCluster
 
+SOURCE = Path("data") / "processed" 
+DEST = Path("models")
+
+def drop_unnamed(df: pd.DataFrame) -> pd.DataFrame:
+    drop_vars = ["crop", "rcp"]
+    df = df.drop(columns=drop_vars, axis=1, errors='ignore')
+    return df.loc[:,~df.columns.str.match("Unnamed")]
+
+
 if __name__ == '__main__':
+    random.seed(42)
+
     c = Client()
 
-    X_train = pd.read_csv('x_train.csv')
-    X_test = pd.read_csv('x_test.csv')
-    y_train = pd.read_csv('y_train.csv')
-    y_test = pd.read_csv('y_test.csv')
-    y_train_n2o = pd.read_csv('y_train_n2o.csv')
-    y_train_dC = pd.read_csv('y_train_dC.csv')
-    
+    X_train = pd.read_csv(SOURCE / 'x_train.csv.gz')
+    X_test = pd.read_csv(SOURCE / 'x_test.csv.gz')
+    y_train = pd.read_csv(SOURCE / 'y_train.csv.gz')
+    y_test = pd.read_csv(SOURCE / 'y_test.csv.gz')
+    y_train_n2o = pd.read_csv(SOURCE / 'y_train_n2o.csv.gz')
+    y_train_gwp = pd.read_csv(SOURCE / 'y_train_gwp.csv.gz')
     
     # assert that columns are in identical order
     # if index in files
@@ -37,47 +47,46 @@ if __name__ == '__main__':
         assert np.array_equal(X_train.iloc[0:5,0], y_train.iloc[0:5,0]), "Order of train and test files does not match!"
         
     
-    def drop_unnamed(df):
-        return df.loc[:,~df.columns.str.match("Unnamed")]
-    
     X_train = drop_unnamed(X_train)
     X_test = drop_unnamed(X_test) 
     y_train = drop_unnamed(y_train) 
     y_test = drop_unnamed(y_test) 
     y_train_n2o = drop_unnamed(y_train_n2o)
-    y_train_dC = drop_unnamed(y_train_dC)
+    y_train_gwp = drop_unnamed(y_train_gwp)
     
     
     frac = 5 # 1/10th
-    ss = random.sample(range(len(X_train)), len(X_train)//frac)
-    X_train = X_train.iloc[ss]
-    y_train = y_train.iloc[ss]
-    y_train_n2o = y_train_n2o.iloc[ss]
-    y_train_dC = y_train_dC.iloc[ss]
+    sample_row_ids = random.sample(range(len(X_train)), len(X_train)//frac)
+    X_train = X_train.iloc[sample_row_ids, :]
+    y_train = y_train.iloc[sample_row_ids, :]
+    y_train_n2o = y_train_n2o.iloc[sample_row_ids, :]
+    y_train_gwp = y_train_gwp.iloc[sample_row_ids, :]
     
-
+    print(X_train.head())
     
     # make pipeline from pre-processing steps and final model
     pipeline_n2o = Pipeline([
-        ("one_hot", OneHotEncoder(cols=["scen", "species"])),
+        ("one_hot", OneHotEncoder(cols=["mana"])),
         #("freq_encode_species", CountEncoder(cols=["species"])),
         ("rf", RandomForestRegressor(
             verbose=True, 
             n_estimators=500,
-            max_features=15,
+            max_features=10,
             max_depth=15)),
     ])
 
     with joblib.parallel_backend("dask"):
         pipeline_n2o.fit(X_train, y_train_n2o)
-        score = pipeline_n2o.score(X_test, y_test['aN2O'])
+        score = pipeline_n2o.score(X_test, y_test['n2o'])
         print(f"SCORE N2O: {score:.2f}")
         
     # save models
-    joblib.dump(pipeline_n2o, "model_n2o_v3.pkl.z")
+    joblib.dump(pipeline_n2o, (DEST / "model_n2o.pkl.z").resolve())
+
+    exit()
 
     # make pipeline from pre-processing steps and final model
-    pipeline_dC = Pipeline([
+    pipeline_gwp = Pipeline([
         ("one_hot", OneHotEncoder(cols=["scen", "species"])),
         #("freq_encode_species", CountEncoder(cols=["species"])),
         ("rf", RandomForestRegressor(
@@ -89,8 +98,8 @@ if __name__ == '__main__':
 
     with joblib.parallel_backend("dask"):
         pipeline_dC.fit(X_train, y_train_dC)
-        score = pipeline_dC.score(X_test, y_test['adC'])
+        score = pipeline_dC.score(X_test, y_test['gwp'])
         print(f"SCORE DC: {score:.2f}")
 
-    joblib.dump(pipeline_dC, "model_dC_v3.pkl.z")
+    joblib.dump(pipeline_gwp, "model_gwp.pkl.z")
 
